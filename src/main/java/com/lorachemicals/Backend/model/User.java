aa9351bcf0a279792e7dec20e219769b27bc95f6
Thamindu Wijerathne <140165111+Thamindu-wijerathne@users.noreply.github.com>
2025-06-13 01:13:41 +0530

    added role to backend

--- src/main/java/com/lorachemicals/Backend/model/User.java
+++ src/main/java/com/lorachemicals/Backend/model/User.java
@@ -18,6 +18,8 @@ public class User {
 
     private String password;
 
+    private String role;
+
     // Default constructor
     public User() {}
 
@@ -32,10 +34,12 @@ public class User {
     public String getName() { return name; }
     public String getEmail() { return email; }
     public String getPassword() { return password; }
+    public String getRole() { return role;}
 
     // Setters
     public void setId(Long id) { this.id = id; }
     public void setName(String name) { this.name = name; }
     public void setEmail(String email) { this.email = email; }
     public void setPassword(String password) { this.password = password; }
+    public void setRole(String role) { this.role = role; }
 }
