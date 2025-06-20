370abc0c4f513b52ca0b4f8c013819f89c304444
Randilachathuranga <randilachathuranga100@gmail.com>
2025-06-20 13:51:50 +0530

    add

--- src/main/java/com/lorachemicals/Backend/controller/ForgotPassword.java
+++ src/main/java/com/lorachemicals/Backend/controller/ForgotPassword.java
@@ -8,6 +8,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.SimpleMailMessage;
+
+
+import java.util.Random;
 
 
 @RestController
@@ -21,11 +26,32 @@ public class ForgotPassword {
         this.userService = userService;
     }
 
+    public String generateOtp() {
+        int otp = 100000 + new Random().nextInt(900000); // 6-digit OTP
+        return String.valueOf(otp);
+    }
+
+    private JavaMailSender mailSender;
+    private void sendOtpEmail(String email, String otp) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(email);
+        message.setSubject("Your OTP for Password Reset");
+        message.setText("Your OTP is: " + otp + "\nThis code will expire in 5 minutes.");
+        mailSender.send(message);
+    }
+
+
     @PostMapping("/forgot-password")
     public ResponseEntity<String> forgotPassword(@RequestBody UserResponseDTO request) {
         User user = userService.findByEmailAndNic(request.getEmail(), request.getNic());
 
         if (user != null) {
+
+            String otp = generateOtp();
+            logger.info("🔐 Generated OTP: {}", otp);
+
+//           sendOtpEmail(request.getEmail(), otp);
+            
             logger.info("✅ User found: {}", user); // log with user info
             return ResponseEntity.ok("User found, reset email can be sent.");
         } else {
@@ -34,6 +60,7 @@ public class ForgotPassword {
         }
     }
 
-    
+
+
 
 }
