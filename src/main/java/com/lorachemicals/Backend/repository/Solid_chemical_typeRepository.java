531343fc40533ea0c9df7f74c4c68c0c55676d62
Randilachathuranga <randilachathuranga100@gmail.com>
2025-06-24 15:04:43 +0530

    add

--- src/main/java/com/lorachemicals/Backend/repository/Solid_chemical_typeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.lorachemicals.Backend.repository;
-
-import com.lorachemicals.Backend.model.Solid_chemical_type;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface Solid_chemical_typeRepository extends JpaRepository<Solid_chemical_type,Long> {
-}
