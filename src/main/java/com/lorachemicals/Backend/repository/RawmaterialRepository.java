4878d5e655269469449bee0864b9e6d033b26d64
Randilachathuranga <randilachathuranga100@gmail.com>
2025-06-26 22:33:46 +0530

    raw material tables is ok

---
+++ src/main/java/com/lorachemicals/Backend/repository/RawmaterialRepository.java
@@ -0,0 +1,7 @@
+package com.lorachemicals.Backend.repository;
+
+import com.lorachemicals.Backend.model.Rawmaterial;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RawmaterialRepository extends JpaRepository<Rawmaterial,Long> {
+}
