8d9111c42bb337e450ae40efb5d4e8c7f102d251
dimuthuh28 <dimuthuharshamal@gmail.com>
2025-06-27 00:08:28 +0530

    Raw Material Model

--- src/main/java/com/lorachemicals/Backend/model/RawMaterial.java
+++ src/main/java/com/lorachemicals/Backend/model/RawMaterial.java
@@ -1,4 +1,14 @@
 package com.lorachemicals.Backend.model;
 
-public class RawMaterial {
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "raw_material")
+@PrimaryKeyJoinColumn(name = "inventoryid")
+@Getter
+@Setter
+public class RawMaterial extends Inventory {
+    // No extra fields, inherits inventoryid from Inventory
 }
