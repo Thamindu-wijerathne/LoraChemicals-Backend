0f6fd76536eef4e503782cc17f73f209459b9390
hasithadil <kah.dilshan@gmail.com>
2025-08-05 12:32:55 +0530

    admin dashboard total sales

--- src/main/java/com/lorachemicals/Backend/controller/AdminDashboardController.java
+++ src/main/java/com/lorachemicals/Backend/controller/AdminDashboardController.java
@@ -3,6 +3,7 @@ package com.lorachemicals.Backend.controller;
 import com.lorachemicals.Backend.dto.AdminDashboardDTO;
 import com.lorachemicals.Backend.services.AdminDashboardService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,5 +18,10 @@ public class AdminDashboardController {
     @GetMapping("/total-orders")
     public AdminDashboardDTO getTotalOrders(){
         return dashboardService.getTotalOrders();
+    }
+
+    @GetMapping("/total-sales")
+    public double getTotalSales(){
+        return dashboardService.getTotalSales();
     }
 }
--- src/main/java/com/lorachemicals/Backend/services/AdminDashboardService.java
+++ src/main/java/com/lorachemicals/Backend/services/AdminDashboardService.java
@@ -1,6 +1,7 @@
 package com.lorachemicals.Backend.services;
 
 import com.lorachemicals.Backend.dto.AdminDashboardDTO;
+import com.lorachemicals.Backend.repository.CustomerOrderItemRepository;
 import com.lorachemicals.Backend.repository.CustomerOrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,9 +11,17 @@ public class AdminDashboardService {
     @Autowired
     private CustomerOrderRepository orderRepository;
 
+
     public AdminDashboardDTO getTotalOrders(){
         long count = orderRepository.count();
         return new AdminDashboardDTO((int) count);
+    }
+
+    @Autowired
+    private CustomerOrderItemRepository orderItemRepository;
+
+    public double getTotalSales(){
+        return orderItemRepository.getTotalSales();
     }
 
 }
