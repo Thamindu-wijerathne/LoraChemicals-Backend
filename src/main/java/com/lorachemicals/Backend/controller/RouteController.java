d751db7f0a0e083e20e6d5dfb599ee06f9334ca3
Randilachathuranga <randilachathuranga100@gmail.com>
2025-10-17 20:05:23 +0530

    add

--- src/main/java/com/lorachemicals/Backend/controller/RouteController.java
+++ src/main/java/com/lorachemicals/Backend/controller/RouteController.java
@@ -1,13 +1,24 @@
 package com.lorachemicals.Backend.controller;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.lorachemicals.Backend.dto.RouteDTO;
 import com.lorachemicals.Backend.model.Route;
 import com.lorachemicals.Backend.services.RouteService;
+import com.lorachemicals.Backend.util.AccessControlUtil;
+
 import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-        import java.util.List;
 
 @RestController
 @RequestMapping("/routes")
